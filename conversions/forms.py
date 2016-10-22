916c7ce3da83e226bd72f1bdc871f020dba90e5b
karlysindy <karlysindy@gmail.com>
2016-10-22 16:09:46 -0400

    Create template folder and sequence input html file

--- conversions/forms.py
+++ conversions/forms.py
@@ -1,9 +1,10 @@
-from django import forms
-from .models import ref_model_input
+#from django import forms
+from django.forms import ModelForm, Textarea
+#from .models import ref_model_input
 
 class RefForm(ModelForm):
     class Meta:
-        model = ref_model_input
+        #model = ref_model_input
         fields = ('reference',)
         widgets = {
             'reference':Textarea(attrs={'placeholder': 'Eg. tatattcaccacatgtaaaactttatttatgcataaaaccaccacacacacacaacctacacaaggaatgtgc agtcctgagtctatttagctacatgtgagtatatactccataaggcatataaaaccagtgcacagaaaatgcatccagatattaatatatctacattttaaaactgcatggaaaatacattattatatatacacaaagtgcatacctacccaatgtatggaaaatatattctgtgagttgtgtttatatacatactgtgtgtgtactaaatacattgaaattgcatt'})
