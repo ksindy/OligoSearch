b6c5dc23b4e43e97c5af14f5c766514d0f317695
karlysindy <karlysindy@gmail.com>
2016-10-22 22:24:36 -0400

    Added rc submit button

--- conversions/views.py
+++ conversions/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from .forms import sequence_input
 
-def test(request):
-    if request.method == "POST":
-        form1 = sequence_input(request.POST, request.FILES)
-    else:
-        form1 = sequence_input()
-    return render (request, 'conversions/conversions_input.html', {'form1': form1})
-
 def reverse_complement(text):
     text = text[::-1].upper().replace(' ','')
     reverse_complement_text = text.translate(str.maketrans('ACGT','TGCA'))
     return reverse_complement_text
+
+def test(request):
+    if request.method == "POST":
+        form1 = sequence_input(request.POST, request.FILES)
+        if(request.GET.get('rc-btn')):
+            reverse_complement(form1)
+        return render(request, 'conversions/rc_output.html')
+    else:
+        form1 = sequence_input()
+    return render (request, 'conversions/conversions_input.html', {'form1': form1})
+
+
 
