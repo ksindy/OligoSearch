a8b94e2e72e7190fefdae2612ce4c6cba60c116b
karlysindy <karlysindy@gmail.com>
2016-10-23 22:26:30 -0400

    added lower

--- conversions/views.py
+++ conversions/views.py
@@ -8,15 +8,20 @@ def reverse_complement(text):
     return reverse_complement_text
 
 def test(request):
-    if (request.POST.get('Upper Case')):
-        print('yes')
-    if (request.POST.get('Reverse Complement')):
-        print('pass if')
-        form1 = sequence_input(request.POST)
+    form1 = sequence_input(request.POST)
+    if request.method == "POST":
         if form1.is_valid():
-            sequence = (form1.cleaned_data['sequence_input']).upper().replace(" ","")
-            rc_result = reverse_complement(sequence)
-        return render(request, 'conversions/conversions_input.html', {'rc_output': rc_result, 'form1': form1})
+            if (request.POST.get('Reverse Complement')):
+                sequence = (form1.cleaned_data['sequence_input'])
+                result = reverse_complement(sequence)
+            if (request.POST.get('Upper Case')):
+                result = (form1.cleaned_data['sequence_input']).upper()
+            if (request.POST.get('Lower Case')):
+                result = (form1.cleaned_data['sequence_input']).lower()
+            if (request.POST.get('Remove Spaces')):
+                result = (form1.cleaned_data['sequence_input']).split(" ","")
+
+        return render(request, 'conversions/conversions_input.html', {'output': result, 'form1': form1})
     else:
         form1 = sequence_input()
     return render (request, 'conversions/conversions_input.html', {'form1': form1})
