8dd2bff981049ec03b37bbe5bdf09e16db589c2c
karlysindy <karlysindy@gmail.com>
2016-10-24 16:09:37 -0400

    removed both model fields now just form fields

--- conversions/forms.py
+++ conversions/forms.py
@@ -1,16 +1,16 @@
 from django import forms
-from django.forms import ModelForm
-from .models import conversions
+#from django.forms import ModelForm
+#from .models import conversions
 
-class sequence_input(ModelForm):
-    class Meta:
-        model = conversions
-        fields = ('sequence_input',)
+# class sequence_input(ModelForm):
+#     class Meta:
+#         model = conversions
+#         fields = ('sequence_input',)
+
+class user_sequence_input(forms.Form):
+    sequence = forms.CharField(label='', widget=forms.Textarea)
 
 
 class pattern_input(forms.Form):
     pattern = forms.CharField(label='', required=False)
-# class pattern_input(ModelForm):
-#     class Meta:
-#         model = conversions
-#         fields = ('pattern_input',)
+
--- conversions/views.py
+++ conversions/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 #from django import forms
-from .forms import sequence_input, pattern_input
+from .forms import user_sequence_input, pattern_input
 
 def reverse_complement(text):
     text = text[::-1].upper().replace(' ','')
@@ -8,30 +8,30 @@ def reverse_complement(text):
     return reverse_complement_text
 
 def test(request):
-    form1 = sequence_input(request.POST)
+    form1 = user_sequence_input(request.POST)
     form2 = pattern_input(request.POST)
     if request.method == "POST":
         if form1.is_valid():
             sequence_list = []
             if (request.POST.get('Reverse Complement')):
-                sequence = (form1.cleaned_data['sequence_input'])
+                sequence = (form1.cleaned_data['sequence'])
                 result = reverse_complement(sequence)
                 sequence_list.append(result)
             if (request.POST.get('Upper Case')):
-                result = (form1.cleaned_data['sequence_input']).upper()
+                result = (form1.cleaned_data['sequence']).upper()
                 sequence_list.append(result)
             if (request.POST.get('Lower Case')):
-                result = (form1.cleaned_data['sequence_input']).lower()
+                result = (form1.cleaned_data['sequence']).lower()
                 sequence_list.append(result)
             if (request.POST.get('Remove Spaces')):
-                result = (form1.cleaned_data['sequence_input']).replace(" ","")
+                result = (form1.cleaned_data['sequence']).replace(" ","")
                 sequence_list.append(result)
         if form2.is_valid():
             print('test')
 
         return render(request, 'conversions/conversions_input.html', {'output': sequence_list, 'form1': form1})
     else:
-        form1 = sequence_input()
+        form1 = user_sequence_input()
     return render (request, 'conversions/conversions_input.html', {'form1': form1, 'form2':form2})
 
 
