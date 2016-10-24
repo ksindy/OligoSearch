1ef057e4c64dea16dceca7a88067a666404ddcd6
karlysindy <karlysindy@gmail.com>
2016-10-24 07:59:43 -0400

    output as list

--- conversions/views.py
+++ conversions/views.py
@@ -11,17 +11,22 @@ def test(request):
     form1 = sequence_input(request.POST)
     if request.method == "POST":
         if form1.is_valid():
+            sequence_list = []
             if (request.POST.get('Reverse Complement')):
                 sequence = (form1.cleaned_data['sequence_input'])
                 result = reverse_complement(sequence)
+                sequence_list.append(result)
             if (request.POST.get('Upper Case')):
                 result = (form1.cleaned_data['sequence_input']).upper()
+                sequence_list.append(result)
             if (request.POST.get('Lower Case')):
                 result = (form1.cleaned_data['sequence_input']).lower()
+                sequence_list.append(result)
             if (request.POST.get('Remove Spaces')):
-                result = (form1.cleaned_data['sequence_input']).split(" ","")
+                result = (form1.cleaned_data['sequence_input']).replace(" ","")
+                sequence_list.append(result)
 
-        return render(request, 'conversions/conversions_input.html', {'output': result, 'form1': form1})
+        return render(request, 'conversions/conversions_input.html', {'output': sequence_list, 'form1': form1})
     else:
         form1 = sequence_input()
     return render (request, 'conversions/conversions_input.html', {'form1': form1})
