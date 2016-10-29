d5bac5e27a213c93832fa16fc806f1eabd00fd99
karlysindy <karlysindy@gmail.com>
2016-10-28 21:10:22 -0400

    ?

--- conversions/forms.py
+++ conversions/forms.py
@@ -6,4 +6,15 @@ class user_sequence_input(forms.Form):
 class pattern_input(forms.Form):
     pattern = forms.CharField(label='', required=False)
 
+mismatch_choices = (
+    (0, 0),
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+)
+
+class mismatch_input(forms.Form):
+    mismatches = forms.ChoiceField(choices=mismatch_choices, required=True, label='Number of mismatches allowed:')
 
