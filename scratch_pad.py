9899857df703a8a9bace2af63d817b0c47079a18
ksindy <karlysindy@gmail.com>
2017-02-17 22:59:16 -0500

    prez

--- scratch_pad.py
+++ scratch_pad.py
@@ -1,43 +1,79 @@
-def find_in(reference, pattern):
-    if pattern in reference:
-        return 'yes'
+import numpy as np
+from matplotlib import animation, rc
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
 
 
-list = ['GTACACCGTGGG']
-final_list = []
-working_list = []
-
-def neighbors(list, mismatch_num):
-    nucleotides = 'ACGT'
-    current_list = []
-
-    for pattern in list:
-        pattern_whole = ''
-        for index, nucleotide in enumerate(pattern):
-            rest = pattern[(index+1)::]
-
-            for i, nuc in enumerate(nucleotides):
-                working_pattern = ''
-                working_pattern += pattern_whole
-                working_pattern += nuc
-                working_pattern += rest
-
-                if working_pattern not in final_list:
-                    final_list.append(working_pattern)
-                    current_list.append(working_pattern)
-                    print(final_list)
-            pattern_whole += nucleotide
-
-    if mismatch_num > 1:
-        for item in current_list:
-            working_list.append(item)
-        return neighbors(working_list, mismatch_num-1)
-    else:
-        return 'this is result{}'.format("\n".join(final_list))
+# import xlrd
+# import re
+# #file = open('/Users/ksindy/PycharmProjects/oligo_search_website/KH Oligos (1) .xlsx')
+# book = xlrd.open_workbook('/Users/ksindy/PycharmProjects/oligo_search_website/KH Oligos (1) .xlsx')
+# sheet = book.sheet_by_index(0)
+# oligo_list = []
+#
+# for i in range(sheet.nrows):
+#     oligo = (sheet.cell_value(
+#         rowx=i,
+#         colx= 2)).upper().replace(" ", "")
+#     regex = re.compile('[^agctuAGCTU]')
+#     oligo = regex.sub('', oligo)
+#     if oligo:
+#         oligo_list.append(oligo)
+# print (oligo_list)
 
 
 
-print(neighbors(list, 2))
+
+    # if i < sheet.nrows and oligo != "" and mismatches_choice != 0:
+    #     name = sheet.cell_value(rowx=i, colx=int(name_column_input))
+    #     name_match = str(name)
+
+
+# def find_in(reference, pattern):
+#     if pattern in reference:
+#         return 'yes'
+#
+#
+# list = ['GTACACCGTGGG']
+# final_list = []
+# working_list = []
+#
+# def neighbors(list, mismatch_num):
+#     nucleotides = 'ACGT'
+#     current_list = []
+#
+#     for pattern in list:
+#         pattern_whole = ''
+#         for index, nucleotide in enumerate(pattern):
+#             rest = pattern[(index+1)::]
+#
+#             for i, nuc in enumerate(nucleotides):
+#                 working_pattern = ''
+#                 working_pattern += pattern_whole
+#                 working_pattern += nuc
+#                 working_pattern += rest
+#
+#                 if working_pattern not in final_list:
+#                     final_list.append(working_pattern)
+#                     current_list.append(working_pattern)
+#                     print(final_list)
+#             pattern_whole += nucleotide
+#
+#     if mismatch_num > 1:
+#         for item in current_list:
+#             working_list.append(item)
+#         return neighbors(working_list, mismatch_num-1)
+#     else:
+#         return 'this is result{}'.format("\n".join(final_list))
+#
+#
+#
+# print(neighbors(list, 2))
 
 # def neighbours (pattern, d):
 #     if d == 0:
